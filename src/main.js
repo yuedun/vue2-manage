c4f726ea693aee07b36fb06ddd6d8fbefba28f01
yuedun <light_v@163.com>
2017-08-04 16:35:03 +0800

    说明性文字

--- src/main.js
+++ src/main.js
@@ -11,3 +11,10 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+/**
+ * 创建一个vue实例，
+ * el:index.html中和App.vue中都包含#app，但是App.vue中的模板会完全替换index.html内容
+ * template: 模板，可以认为以“.vue”结尾的文件都是模板，主要使用了App.vue中的html内容，用于承载组建内容，在js中直接使用
+ * 导入的变量名作为模板名，在vue文件中使用注册的name的值，标签不区分大小写
+ * components: 主要使用了script内容
+ */
